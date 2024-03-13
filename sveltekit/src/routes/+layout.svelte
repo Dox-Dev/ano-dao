d59f26209495f51179f2afddef34ad65e91b85cd
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-13 21:50:14 +0800

    feat: add `AppBar` above and `AppRail` on the side

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	// App Rail (sidebar)
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+	let currentTile: number = 0;
 </script>
 
 <!-- App Shell -->
@@ -44,6 +48,30 @@
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
+
+	<AppRail>
+		<svelte:fragment slot="lead">
+			<AppRailAnchor href="/" >(icon)</AppRailAnchor>
+		</svelte:fragment>
+		
+		<!-- --- -->
+		<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<span>Tile 1</span>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<span>Tile 2</span>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<span>Tile 3</span>
+		</AppRailTile>
+		<!-- --- -->
+		<svelte:fragment slot="trail">
+			<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+		</svelte:fragment>
+	</AppRail>
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
