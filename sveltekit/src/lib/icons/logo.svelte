2f14491c3f2627bb070698c865119d30789c8b19
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-14 01:19:14 +0800

    style: made eyes and mouth of logo emphasized in dark mode

--- sveltekit/src/lib/icons/logo.svelte
+++ sveltekit/src/lib/icons/logo.svelte
@@ -1,8 +1,12 @@
+<style>
+    .fill-token { stroke: 'rgba(var(--theme-font-color-base))' }
+    .dark .fill-token { stroke: 'rgba(var(--theme-font-~~c~~olor-dark))' }
+</style>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48" height="48" width="48" id="Confused-Nick-Young--Streamline-Memes">
     <desc>Confused Nick Young Streamline Element: https://streamlinehq.com</desc>
-    <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M21.443 24.276c3.256 -0.851 3.63 -3.01 3.271 -4.122 -0.088 -0.274 -0.419 -0.31 -0.647 -0.137 -1.726 1.299 -4.688 2.295 -7.233 2.168 -0.318 -0.016 -0.577 0.245 -0.443 0.534 0.467 1.01 1.923 2.375 5.052 1.557Z" stroke-width="1"></path>
-    <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="m19.63 21.94 0.589 2.56" stroke-width="1"></path>
-    <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="m22.063 21.156 0.812 2.578" stroke-width="1"></path>
+    <path class="fill-token" stroke-linecap="round" stroke-linejoin="round" d="M21.443 24.276c3.256 -0.851 3.63 -3.01 3.271 -4.122 -0.088 -0.274 -0.419 -0.31 -0.647 -0.137 -1.726 1.299 -4.688 2.295 -7.233 2.168 -0.318 -0.016 -0.577 0.245 -0.443 0.534 0.467 1.01 1.923 2.375 5.052 1.557Z" stroke-width="1"></path>
+    <path class="fill-token" stroke-linecap="round" stroke-linejoin="round" d="m19.63 21.94 0.589 2.56" stroke-width="1"></path>
+    <path class="fill-token" stroke-linecap="round" stroke-linejoin="round" d="m22.063 21.156 0.812 2.578" stroke-width="1"></path>
     <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="M16.458 28.563c1.8 0.633 3.759 1.462 5.693 0.987 2.402 -0.59 4.542 -3.638 5.74 -5.686 0.703 -1.201 0.808 -2.64 0.438 -3.99L24.95 7.543C23.71 3.006 19.014 0.294 14.465 1.484a8.398 8.398 0 0 0 -5.99 10.367l3.37 12.3c0.282 1.033 0.831 1.974 1.619 2.673l2.156 5.345C11.432 34.44 2.858 39.857 2.058 43.357a29.018 29.018 0 0 0 -0.62 3.721" stroke-width="1"></path>
     <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="m24.243 10.584 -0.957 -0.22a0.5 0.5 0 0 0 -0.43 0.1l-1.97 1.627a0.428 0.428 0 0 1 -0.274 0.104c-0.472 -0.004 -1.123 -0.208 -0.838 -1.031" stroke-width="1"></path>
     <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" d="m11.774 13.856 0.716 -0.657a0.5 0.5 0 0 1 0.425 -0.123l2.538 0.447a0.43 0.43 0 0 0 0.29 -0.043c0.407 -0.234 0.866 -0.729 0.199 -1.302" stroke-width="1"></path>
