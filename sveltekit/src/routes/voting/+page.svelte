2d8a8e874b5c61b5df0970b928fff01acf4b40a3
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-14 09:25:33 +0800

    feat(`voting`): make temporary layout for voting

---
+++ sveltekit/src/routes/voting/+page.svelte
@@ -0,0 +1,27 @@
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+        <!-- Card -->
+        <div class="card p-4">
+            <!-- Question/Proposal -->
+            <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
+
+            <!-- Options -->
+            <div class="space-y-2">
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" checked name="radio-direct" value="1" />
+            		<p>Option 1</p>
+            	</label>
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" name="radio-direct" value="2" />
+            		<p>Option 2</p>
+            	</label>
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" name="radio-direct" value="3" />
+            		<p>Option 3</p>
+            	</label>
+            </div>
+        </div>
+    </div>
+</div>
