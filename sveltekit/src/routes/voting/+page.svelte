3cc3ea2d86351e56ff97d9222c5d08377701e92c
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-15 17:23:03 +0800

    style(`voting`): make cards more appealing

--- sveltekit/src/routes/voting/+page.svelte
+++ sveltekit/src/routes/voting/+page.svelte
@@ -7,65 +7,71 @@
 		<!-- Card -->
         <div class="card p-4 size-1/2">
             <!-- Question/Proposal -->
-            <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
+            <section class="p-4">Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</section>
 
             <!-- Options -->
-            <div class="space-y-2">
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" checked name="radio-direct" value="1" />
-            		<p>Option 1</p>
-            	</label>
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" name="radio-direct" value="2" />
-            		<p>Option 2</p>
-            	</label>
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" name="radio-direct" value="3" />
-            		<p>Option 3</p>
-            	</label>
-            </div>
+			<footer class="card-footer">
+            	<div class="space-y-2">
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" checked name="radio-direct" value="1" />
+            			<p>Option 1</p>
+            		</label>
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" name="radio-direct" value="2" />
+            			<p>Option 2</p>
+            		</label>
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" name="radio-direct" value="3" />
+            			<p>Option 3</p>
+            		</label>
+            	</div>
+			</footer>
         </div>
 		
 		<div class="card p-4 size-1/2">
             <!-- Question/Proposal -->
-            <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
+            <section class="p-4">Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</section>
 
             <!-- Options -->
-            <div class="space-y-2">
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" checked name="radio-direct" value="1" />
-            		<p>Option 1</p>
-            	</label>
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" name="radio-direct" value="2" />
-            		<p>Option 2</p>
-            	</label>
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" name="radio-direct" value="3" />
-            		<p>Option 3</p>
-            	</label>
-            </div>
+			<footer class="card-footer">
+            	<div class="space-y-2">
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" checked name="radio-direct" value="1" />
+            			<p>Option 1</p>
+            		</label>
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" name="radio-direct" value="2" />
+            			<p>Option 2</p>
+            		</label>
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" name="radio-direct" value="3" />
+            			<p>Option 3</p>
+            		</label>
+            	</div>
+			</footer>
         </div>
 
 		<div class="card p-4 size-1/2">
             <!-- Question/Proposal -->
-            <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
+            <section class="p-4">Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</section>
 
             <!-- Options -->
-            <div class="space-y-2">
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" checked name="radio-direct" value="1" />
-            		<p>Option 1</p>
-            	</label>
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" name="radio-direct" value="2" />
-            		<p>Option 2</p>
-            	</label>
-            	<label class="flex items-center space-x-2">
-            		<input class="radio" type="radio" name="radio-direct" value="3" />
-            		<p>Option 3</p>
-            	</label>
-            </div>
+			<footer class="card-footer">
+            	<div class="space-y-2">
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" checked name="radio-direct" value="1" />
+            			<p>Option 1</p>
+            		</label>
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" name="radio-direct" value="2" />
+            			<p>Option 2</p>
+            		</label>
+            		<label class="flex items-center space-x-2">
+            			<input class="radio" type="radio" name="radio-direct" value="3" />
+            			<p>Option 3</p>
+            		</label>
+            	</div>
+			</footer>
         </div>
 
 		<div class="p-2"/> <!-- space below last card-->
