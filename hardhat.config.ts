3431af0d8c35aadf8eb6da4a14374bf08fdc8543
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-15 12:34:23 +0800

    build: make the DAO and token deployable

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "dotenv/config"
+
+const ARBITRUM_SEPOLIA_URL: string = process.env.ARBITRUM_SEPOLIA_URL ?? ""; // added ?? "" for null safety (since value can possibly be null)
+const WALLET_PRIVATE_KEY: string = process.env.WALLET_PRIVATE_KEY ?? "";
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+
+  networks:{
+    // network name here was used in `package.json`
+    // particularly in the `hhcompile` script
+    arbitrumSepolia: {
+      url: ARBITRUM_SEPOLIA_URL,  // https://chainlist.org/chain/421614 - check here what RPCs are available
+      accounts: [WALLET_PRIVATE_KEY], // people who will pay for txn (private key)
+                                      // Important: These should be ENVIRONMENT VARIABLES
+      chainId: 421614,  // specific for other layers of ETH chain
+                        // chainId = L2 (layer2) of Ethereum
+    },
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,31 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  //const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  //const unlockTime = currentTimestampInSeconds + 60;
+//
+  //const lockedAmount = ethers.parseEther("0.001");
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const token = await ethers.deployContract("AnoTher", [
+    "AnoTher",
+    "AnoT",
+    10000,
+  ]);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await token.waitForDeployment();
 
-  await lock.waitForDeployment();
+  console.log(`deployed to ${token.target}`);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const dao = await ethers.deployContract("AnoDAO", [token.target, 100]);
+  await dao.waitForDeployment();
+
+  console.log(`deployed to ${dao.target}`);
+
+  //console.log(
+  //  `Lock with ${ethers.formatEther(
+  //    lockedAmount
+  //  )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  //);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
