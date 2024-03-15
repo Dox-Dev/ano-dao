f63faa26ae2f2553116ca9b2e647e214cf4bea90
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-15 17:18:45 +0800

    feat(`voting`): make proposals scrollable

--- sveltekit/src/routes/voting/+page.svelte
+++ sveltekit/src/routes/voting/+page.svelte
@@ -1,9 +1,11 @@
 
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-        <!-- Card -->
-        <div class="card p-4">
+        <div class="p-2"/> <!-- space above 1st card-->
+		
+		<!-- Card -->
+        <div class="card p-4 size-1/2">
             <!-- Question/Proposal -->
             <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
 
@@ -23,5 +25,49 @@
             	</label>
             </div>
         </div>
+		
+		<div class="card p-4 size-1/2">
+            <!-- Question/Proposal -->
+            <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
+
+            <!-- Options -->
+            <div class="space-y-2">
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" checked name="radio-direct" value="1" />
+            		<p>Option 1</p>
+            	</label>
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" name="radio-direct" value="2" />
+            		<p>Option 2</p>
+            	</label>
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" name="radio-direct" value="3" />
+            		<p>Option 3</p>
+            	</label>
+            </div>
+        </div>
+
+		<div class="card p-4 size-1/2">
+            <!-- Question/Proposal -->
+            <div>Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</div>
+
+            <!-- Options -->
+            <div class="space-y-2">
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" checked name="radio-direct" value="1" />
+            		<p>Option 1</p>
+            	</label>
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" name="radio-direct" value="2" />
+            		<p>Option 2</p>
+            	</label>
+            	<label class="flex items-center space-x-2">
+            		<input class="radio" type="radio" name="radio-direct" value="3" />
+            		<p>Option 3</p>
+            	</label>
+            </div>
+        </div>
+
+		<div class="p-2"/> <!-- space below last card-->
     </div>
 </div>
