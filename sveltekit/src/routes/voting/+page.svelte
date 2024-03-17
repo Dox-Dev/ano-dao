b5500be4f3ca179f0486637544833e8167c12373
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-17 15:26:21 +0800

    feat(`voting`): connect to contract

--- sveltekit/src/routes/voting/+page.svelte
+++ sveltekit/src/routes/voting/+page.svelte
@@ -1,9 +1,63 @@
+<script lang="ts">
+	import {AnoDAO__factory} from "../../../../typechain-types";
+	import type {AnoDAO} from "../../../../typechain-types";
+	
+	//import useState from 'hooks.js';
+	import {ethers} from 'ethers';
+	import AnoDAO_json from "../../../../artifacts/contracts/AnoDAO.sol/AnoDAO.json";
+	import 'dotenv';
 
+	import {
+		PUBLIC_DAO_ADDRESS,
+	} from '$env/static/public';
+
+	// The contract address
+	const DAO_ADDRESS = PUBLIC_DAO_ADDRESS;
+	console.log(DAO_ADDRESS);
+
+	// Property variables
+	
+
+	async function requestAccount() {
+		await window.ethereum.request({ method: "eth_requestAccounts" });
+	}
+
+	// Create a proposal
+	async function createProposal() {
+		// check if Metamask extension exists
+		if(typeof window.ethereum !== "undefined") {
+			const provider = await new ethers.BrowserProvider(window.ethereum);
+			const signer = await provider.getSigner();
+			const contract = new ethers.Contract(
+				DAO_ADDRESS, 
+				AnoDAO_json.abi,
+				signer
+			);
+			try { // try to create the proposal
+				const res = await contract.createProposal("proposal1", "description1");
+				console.log("result: ", res);
+			} catch (error) {
+				console.log("error: ", error);
+			}
+		}
+	}
+
+	// Vote
+
+	// Set proposal required price
+
+	// Be able to vote on proposal
+	//user.vote()
+
+</script>
 
 <div class="container mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
         <div class="p-2"/> <!-- space above 1st card-->
 		
+		<button on:click={requestAccount}>Connect Account</button>
+		<button on:click={createProposal}>test</button>
+
 		<!-- Card -->
         <div class="card p-4 size-1/2">
             <!-- Question/Proposal -->
