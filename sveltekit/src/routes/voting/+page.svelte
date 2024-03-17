a5470c06ad3e09bc711ab54a84b2258b027ba56e
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-17 21:28:50 +0800

    chore(`voting`): remove unnecessary and unused components

--- sveltekit/src/routes/voting/+page.svelte
+++ sveltekit/src/routes/voting/+page.svelte
@@ -142,168 +142,89 @@
 	//user.vote()
 
 </script>
-
-<form method="POST" class="m-5" on:submit|preventDefault={createAProposal}>
-	<h1 class="h1">Create a proposal</h1>
-	<label class="label mt-3">
-		<span class="h2">Title</span>
-		<input name="title" bind:value={title} class="input" type="text" placeholder="Write your title here."/>
-	</label>
-	<label class="label mt-3">
-		<span class="h2">Description</span>
-		<textarea name="description" bind:value={description} class="textarea" rows="4" placeholder="Write your description here."/>
-	</label>
-	<button type="submit" class="btn btn-sm variant-filled mt-2">Submit</button>
-</form>
-
-<form method="POST" class="m-5 mt-10" on:submit|preventDefault={voteOnProposal}>
-	<h1 class="h1">Vote on a proposal</h1>
-	<label class="label mt-3">
-		<span class="h2">Proposal ID</span>
-		<input name="proposalID" bind:value={proposalID} class="input" type="text" placeholder="Write your proposal ID here."/>
-	</label>
-	<span><button type="button" on:click={viewProposal} class="btn btn-sm variant-filled mt-2">View</button>
-		<button type="button" on:click={hideProposal} class="btn btn-sm variant-filled mt-2">Hide</button></span>
-	{#if viewedCreator && viewedTitle && viewedDescription}
-	<!-- Note: I didn't check for viewedYesVotes and viewedNoVotes
-		 since there could be instances where no one has voted yet-->
-		<div class="card p-4">
-			<header class="card-header text-4xl">
-				{viewedTitle}
-			</header>
-
-			<!-- Question/Proposal -->
-			<section class="p-4">
-				<!-- <div>CREATOR: {viewedCreator}</div> -->
-				<div>{viewedDescription}</div>
-				<div>
-					<i class="fa-solid fa-thumbs-up"/> {viewedYesVotes}
-					<i class="fa-solid fa-thumbs-down"/> {viewedNoVotes}
-				</div>
-			</section>
-
-			<!-- Options -->
-			<footer class="p-4 flex justify-start items-center space-x-4">
-				<span>
-					<div class="flex-auto flex justify-between items-center">
-						<div class="mr-2">
-						<Identicon
-						seed={creatorIdenticonHash}
-						height={5}
-						width={5}
-						pixelSize={5}
-						numberOfColors={2}
-						symetry="central"
-						text={undefined}
-						textColor="#ffffff"
-						/>
-						</div>
-					
-						{viewedCreator}
+<div class="my-5">
+	<form method="POST" class="m-5" on:submit|preventDefault={createAProposal}>
+		<h1 class="h1">Create a proposal</h1>
+		<label class="label mt-3">
+			<span class="h2">Title</span>
+			<input name="title" bind:value={title} class="input" type="text" placeholder="Write your title here."/>
+		</label>
+		<label class="label mt-3">
+			<span class="h2">Description</span>
+			<textarea name="description" bind:value={description} class="textarea" rows="4" placeholder="Write your description here."/>
+		</label>
+		<button type="submit" class="btn btn-sm variant-filled mt-2">Submit</button>
+	</form>
+	
+	<form method="POST" class="m-5 mt-10" on:submit|preventDefault={voteOnProposal}>
+		<h1 class="h1">Vote on a proposal</h1>
+		<label class="label mt-3">
+			<span class="h2">Proposal ID</span>
+			<input name="proposalID" bind:value={proposalID} class="input" type="text" placeholder="Write your proposal ID here."/>
+		</label>
+		<span><button type="button" on:click={viewProposal} class="btn btn-sm variant-filled mt-2">View</button>
+			<button type="button" on:click={hideProposal} class="btn btn-sm variant-filled mt-2">Hide</button></span>
+		{#if viewedCreator && viewedTitle && viewedDescription}
+		<!-- Note: I didn't check for viewedYesVotes and viewedNoVotes
+			 since there could be instances where no one has voted yet-->
+			<div class="card p-4">
+				<header class="card-header text-4xl">
+					{viewedTitle}
+				</header>
+			
+				<!-- Question/Proposal -->
+				<section class="p-4">
+					<!-- <div>CREATOR: {viewedCreator}</div> -->
+					<div>{viewedDescription}</div>
+					<div>
+						<i class="fa-solid fa-thumbs-up"/> {viewedYesVotes}
+						<i class="fa-solid fa-thumbs-down"/> {viewedNoVotes}
 					</div>
-				</span>
-			</footer>
+				</section>
+			
+				<!-- Options -->
+				<footer class="p-4 flex justify-start items-center space-x-4">
+					<span>
+						<div class="flex-auto flex justify-between items-center">
+							<div class="mr-2">
+							<Identicon
+							seed={creatorIdenticonHash}
+							height={5}
+							width={5}
+							pixelSize={5}
+							numberOfColors={2}
+							symetry="central"
+							text={undefined}
+							textColor="#ffffff"
+							/>
+							</div>
+						
+							{viewedCreator}
+						</div>
+					</span>
+				</footer>
+			</div>
+		{/if}
+		<div class="space-y-2 mt-3">
+			<span class="h2">Yes/No?</span>
+			<label class="flex items-center space-x-2">
+				<input bind:group={vote} value={true} class="radio" type="radio" checked name="radio-direct" />
+				<p>Yes</p>
+			</label>
+			<label class="flex items-center space-x-2">
+				<input bind:group={vote} value={false} class="radio" type="radio" name="radio-direct" />
+				<p>No</p>
+			</label>
 		</div>
-	{/if}
-	<div class="space-y-2 mt-3">
-		<span class="h2">Yes/No?</span>
-		<label class="flex items-center space-x-2">
-			<input bind:group={vote} value={true} class="radio" type="radio" checked name="radio-direct" />
-			<p>Yes</p>
+		<button type="submit" class="btn btn-sm variant-filled mt-2">Submit</button>
+	</form>
+	
+	<form method="POST" class="m-5 mt-10" on:submit|preventDefault={setProposalPrice}>
+		<h1 class="h1">Set proposal creation price</h1>
+		<label class="label mt-3">
+			<span class="h2">Proposal Price</span>
+			<input name="proposalPrice" bind:value={proposalPrice} class="input" type="text" placeholder="Set a price for users to create proposals here."/>
 		</label>
-		<label class="flex items-center space-x-2">
-			<input bind:group={vote} value={false} class="radio" type="radio" name="radio-direct" />
-			<p>No</p>
-		</label>
-	</div>
-	<button type="submit" class="btn btn-sm variant-filled mt-2">Submit</button>
-</form>
-
-<form method="POST" class="m-5 mt-10" on:submit|preventDefault={setProposalPrice}>
-	<h1 class="h1">Set proposal creation price</h1>
-	<label class="label mt-3">
-		<span class="h2">Proposal Price</span>
-		<input name="proposalPrice" bind:value={proposalPrice} class="input" type="text" placeholder="Set a price for users to create proposals here."/>
-	</label>
-	<button type="submit" class="btn btn-sm variant-filled mt-2">Submit</button>
-</form>
-
-<div class="container mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
-        <div class="p-2"/> <!-- space above 1st card-->
-		
-		<!-- <button on:click={requestAccount}>Connect Account</button> -->
-
-		<!-- Card -->
-        <div class="card p-4 size-1/2">
-            <!-- Question/Proposal -->
-            <section class="p-4">Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</section>
-
-            <!-- Options -->
-			<footer class="card-footer">
-            	<div class="space-y-2">
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" checked name="radio-direct" value="1" />
-            			<p>Option 1</p>
-            		</label>
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" name="radio-direct" value="2" />
-            			<p>Option 2</p>
-            		</label>
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" name="radio-direct" value="3" />
-            			<p>Option 3</p>
-            		</label>
-            	</div>
-			</footer>
-        </div>
-		
-		<div class="card p-4 size-1/2">
-            <!-- Question/Proposal -->
-            <section class="p-4">Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</section>
-
-            <!-- Options -->
-			<footer class="card-footer">
-            	<div class="space-y-2">
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" checked name="radio-direct" value="1" />
-            			<p>Option 1</p>
-            		</label>
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" name="radio-direct" value="2" />
-            			<p>Option 2</p>
-            		</label>
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" name="radio-direct" value="3" />
-            			<p>Option 3</p>
-            		</label>
-            	</div>
-			</footer>
-        </div>
-
-		<div class="card p-4 size-1/2">
-            <!-- Question/Proposal -->
-            <section class="p-4">Insert proposal here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, maiores! Sunt nulla id dolores ipsam molestiae eos voluptate. Repellendus voluptatibus odit, voluptates at sunt ipsam reiciendis cumque culpa maiores rerum?</section>
-
-            <!-- Options -->
-			<footer class="card-footer">
-            	<div class="space-y-2">
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" checked name="radio-direct" value="1" />
-            			<p>Option 1</p>
-            		</label>
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" name="radio-direct" value="2" />
-            			<p>Option 2</p>
-            		</label>
-            		<label class="flex items-center space-x-2">
-            			<input class="radio" type="radio" name="radio-direct" value="3" />
-            			<p>Option 3</p>
-            		</label>
-            	</div>
-			</footer>
-        </div>
-
-		<div class="p-2"/> <!-- space below last card-->
-    </div>
+		<button type="submit" class="btn btn-sm variant-filled mt-2">Submit</button>
+	</form>
 </div>
